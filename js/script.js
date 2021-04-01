2d3f1ed737bb6fd792812b2ac52f4f5b2e659288
Cameron <camo3223@gmail.com>
2021-04-01 16:40:39 +1100

    Built UI and basic functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function add(a, b){
     return a + b;
 }
@@ -14,6 +15,44 @@ function divide(a, b){
     return a / b;
 }
 
+function clearCalculation() {
+    calculation = "";
+    display.textContent = calculation;
+}
+
 function operate(a, b, operator) {
     return operator(a,b);    
 }
+
+// Configs
+const calculationLimit = 9;
+var calculation = "";
+const display = document.querySelector(".calculator-display");
+
+//Listener Events
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () => {
+    clearCalculation();
+})
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+        if(calculation === "ERROR") clearCalculation();
+
+        display.textContent += button.value;
+        calculation += button.value;
+
+        if(calculation.length > calculationLimit) {
+            calculation = "ERROR";
+            display.textContent = calculation;
+        }
+    })
+})
+
+
+const equalsButton = document.querySelector(".equals");
+equalsButton.addEventListener('click', () => {
+    // Parse Calculation and throw to operate(num1, num2, operator);
+})
