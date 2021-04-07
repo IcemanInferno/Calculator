8eee250953da7b43f93b6b981016a2a6b2def7b8
Cameron <camo3223@gmail.com>
2021-04-07 16:55:14 +1000

    Complete, but could add Keyboard functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// Operator Functions
 function add(a, b){
     return a + b;
 }
@@ -12,47 +12,129 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if(a === 0 || b === 0) alert("Don't divide by 0!")
     return a / b;
 }
 
-function clearCalculation() {
-    calculation = "";
-    display.textContent = calculation;
-}
-
 function operate(a, b, operator) {
-    return operator(a,b);    
+    switch (operator) {
+        case "add":
+            return add(a,b);
+        case "subtract":
+            return subtract(a,b);
+        case "multiply":
+            return multiply(a,b);
+        case "divide":
+            return divide(a,b);
+        default:
+            return "UNKNOWN";
+    }
 }
 
-// Configs
-const calculationLimit = 9;
-var calculation = "";
-const display = document.querySelector(".calculator-display");
+// Functions
+function updateDisplay() {
+    if((firstOperand.length + secondOperand.length) > OperandLimit) {
+        clearDisplay();
+        alert("The value you have entered is too long")
+    }
 
-//Listener Events
+    if(firstOperand === "-") firstOperand = "";
+    if(secondOperand === "-") secondOperand = "";
+    if(displayValue === "-") displayValue = "";
+    
+    const display = document.querySelector(".calculator-display");
+    display.textContent = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    firstOperand = "";
+    secondOperand = "";
+    operator = "";
+    updateDisplay();
+}
+
+function performCalculation() {
+    let result = operate(parseFloat(firstOperand), parseFloat(secondOperand), operator);
+    result = String(Math.round(result * 100) / 100);
+
+    clearDisplay();
+    firstOperand = result;
+    displayValue = result;    
+    updateDisplay();
+}
+
+//Config
+const OperandLimit = 12;
+let displayValue = '0';
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
+
+updateDisplay();
+
+//clear button
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
-    clearCalculation();
+    clearDisplay();
 })
 
-const buttons = document.querySelectorAll('button');
+//numbers
+const buttons = document.querySelectorAll('.number');
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-
-        if(calculation === "ERROR") clearCalculation();
-
-        display.textContent += button.value;
-        calculation += button.value;
-
-        if(calculation.length > calculationLimit) {
-            calculation = "ERROR";
-            display.textContent = calculation;
+    button.addEventListener('click', () => {        
+        if(displayValue == 0) displayValue = "";
+        
+        if((button.value === ".") && (firstOperand.includes(".") || secondOperand.includes("."))){} // Do nothing
+        else{
+            if(!operator) {                             
+                firstOperand += button.value;
+                displayValue += button.value;            
+            }
+            else {                
+                secondOperand += button.value;
+                displayValue += button.value;
+            }
         }
+
+        updateDisplay();
     })
 })
 
+//operators
+const operators = document.querySelectorAll('.operator');
+operators.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(!firstOperand) firstOperand = "0";
+        if(firstOperand && secondOperand && operator){
+            performCalculation();
+        }
 
+        operator = button.value;
+        displayValue += button.textContent;        
+        updateDisplay();
+    })
+})
+
+//backspace
+const backspaceButton = document.querySelector(".backspace");
+backspaceButton.addEventListener('click', () => {
+    if(!operator && firstOperand) {                             
+        firstOperand = firstOperand.slice(0,-1);
+        displayValue = displayValue.slice(0,-1);            
+    }
+    else if(secondOperand) {   
+        secondOperand = String(secondOperand).slice(0,-1);
+        displayValue = String(displayValue).slice(0,-1);
+    }
+
+    updateDisplay();
+})
+
+//equals
 const equalsButton = document.querySelector(".equals");
 equalsButton.addEventListener('click', () => {
-    // Parse Calculation and throw to operate(num1, num2, operator);
+    if(firstOperand && secondOperand && operator) {
+        performCalculation();
+    }
 })
